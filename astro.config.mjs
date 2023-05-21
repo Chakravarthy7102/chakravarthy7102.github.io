b38f639ccd690b544fa6c4145cc4276e067461b9
Chakravarthy7102 <medicharlachakravarthi@gmail.com>
2023-05-21 20:48:10 +0530

    changed some stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site:"https://Chakravarthy7102.github.io",
+  site:"https://chakravarthy7102.github.io",
   base:"/devchaks",
   integrations: [tailwind()]
 });
